5d81ed660bfb845b9702d8e43e0d6d0bb60afbaa
mturner <mjt6yf@umsystem.edu>
2024-02-20 16:29:24 -0600

    completed problem 4

--- comment_filter.cpp
+++ comment_filter.cpp
@@ -1,13 +1,24 @@
 #include<iostream>
 #include<string>
+#include<fstream>
 
-using std::cout;
-using std::endl;
-using std::cin;
+using namespace std;
 
-int main() 
-{
-    // Write your program here.
+int main() {
+    ifstream input_file("story.txt");
+    ofstream output_file("updated_story.txt");
+    string storyLine;
+    
+    while(getline(input_file, storyLine)) {
+        size_t hashtag = storyLine.find('#');
+        if(hashtag != string::npos) { 
+            storyLine[hashtag] = ' '; 
+        }
+        output_file << storyLine << endl;
+    }
+    
+    input_file.close();
+    output_file.close();
 
     return 0;
 }
